52b521691503b567b4c6ee71074e02ab2de3700a
metaldudu <metaldudu@gmail.com>
2022-11-26 23:53:06 +0800

    v.1.0.1

--- F键全屏播放视频.user.js
+++ F键全屏播放视频.user.js
@@ -2,11 +2,14 @@
 // @name        F键全屏播放视频
 // @namespace   Violentmonkey Scripts
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAB7UlEQVRoge2ZTU4CMRiGHxF0dGPUxJ16A6/hT1x4ARGDGGNcaryBW0/gKVAuocYDmICCrt2aAcRFS0SkYGk/Z2L6JN9i6LTf+9KfTFsIBAI+mAFKQAV4AZpARyiaOkdF54x8mdgG6oLCR8UzsOVq4gRoJ2iiGy3geFwT2ykx0Wtm09ZEhOrSpMX3RwM1X3/NYQpEm+JgkOCMwciOjes/ZqA2k5E1QSGuDNQ2YXg5BnJyWpxoAlP9P5qMdGS1OPNDt2louXAFLPwiHnwmzfpsTPMOvAFzwOKQ9yZ9JpUw0mUfuBRs/xsSQysRgpG08W+MSE72G+DVULYOFH0mkzTyqGMQS76TSRo5Ai4MZdO+k0kaiYB5wfa/8W8mezCSNoKRIXQnudUhgSthY5U2gpG0EYykjVFG0rR6DdUyykgJ+PCnZWw+UFqsiVH/QBbYAKokd2hdRW3Ecvo5tjHS0JWW9fMskAeugVqPUYmIdY4ysMvXF8KqLq/bGLnRlfI2lYQpoDSVbSqVdKVbzJ8xf0kGuEdpstrr995YnfrXZc05SkuNMbbJW6h7uzZwRjI9k0GZaGst6+M2dKwb6AB3wB6wguzdSU7nKPA1nFqowwwnNoEnklt+azj0RD8R6hKyjFqapZffhs5VRODoKBCw4BPhMH4TDNDMWAAAAABJRU5ErkJggg==
-// @version     0.1.0
+// @version     0.1.1
+// @author      MetalDudu
 // @match       https://*.thepaper.cn/*
 // @match       https://www.aliyundrive.com/*
 // @match       https://www.miguvideo.com/*
-// @description 支持网站：澎湃、阿里云盘、咪咕视频
+// @match       https://www.douban.com/people/*
+// @match       https://v.youku.com/*
+// @description 支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷
 // @license MIT
 // @description 2022/11/23 10:30:23
 // ==/UserScript==
@@ -29,6 +32,12 @@ const getSiteName = hostname => hostname.match(/([^\.]+)\.[^\.]+$/)[1];
         }
         if(siteName === 'miguvideo'){
           document.querySelector(".btn-bg.zoom-out").click();
+        }
+        if(siteName === 'douban'){
+          document.getElementsByClassName("vjs-fullscreen-control vjs-control vjs-button")[0].click()
+        }
+        if(siteName === 'youku'){
+          document.getElementsByClassName("kui-fullscreen-icon-0")[0].click()
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FullScreen-hotkey
 
-使用 F 快捷键全屏播放视频，支持网站：澎湃、阿里云盘、咪咕视频
+使用 F 快捷键全屏播放视频，支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷
 
 [https://greasyfork.org/zh-CN/scripts/455333](https://greasyfork.org/zh-CN/scripts/455333)
