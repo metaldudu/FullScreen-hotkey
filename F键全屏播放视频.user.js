94e01c705e7a7f602ca00e89c6cdecb7b9cf584c
metaldudu <metaldudu@gmail.com>
2022-12-28 20:48:48 +0800

    add huxiu.com

--- F键全屏播放视频.user.js
+++ F键全屏播放视频.user.js
@@ -2,7 +2,7 @@
 // @name        F键全屏播放视频
 // @namespace   Violentmonkey Scripts
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAB7UlEQVRoge2ZTU4CMRiGHxF0dGPUxJ16A6/hT1x4ARGDGGNcaryBW0/gKVAuocYDmICCrt2aAcRFS0SkYGk/Z2L6JN9i6LTf+9KfTFsIBAI+mAFKQAV4AZpARyiaOkdF54x8mdgG6oLCR8UzsOVq4gRoJ2iiGy3geFwT2ykx0Wtm09ZEhOrSpMX3RwM1X3/NYQpEm+JgkOCMwciOjes/ZqA2k5E1QSGuDNQ2YXg5BnJyWpxoAlP9P5qMdGS1OPNDt2louXAFLPwiHnwmzfpsTPMOvAFzwOKQ9yZ9JpUw0mUfuBRs/xsSQysRgpG08W+MSE72G+DVULYOFH0mkzTyqGMQS76TSRo5Ai4MZdO+k0kaiYB5wfa/8W8mezCSNoKRIXQnudUhgSthY5U2gpG0EYykjVFG0rR6DdUyykgJ+PCnZWw+UFqsiVH/QBbYAKokd2hdRW3Ecvo5tjHS0JWW9fMskAeugVqPUYmIdY4ysMvXF8KqLq/bGLnRlfI2lYQpoDSVbSqVdKVbzJ8xf0kGuEdpstrr995YnfrXZc05SkuNMbbJW6h7uzZwRjI9k0GZaGst6+M2dKwb6AB3wB6wguzdSU7nKPA1nFqowwwnNoEnklt+azj0RD8R6hKyjFqapZffhs5VRODoKBCw4BPhMH4TDNDMWAAAAABJRU5ErkJggg==
-// @version     0.1.3
+// @version     0.1.4
 // @author      MetalDudu
 // @match       https://*.thepaper.cn/*
 // @match       https://www.aliyundrive.com/*
@@ -10,10 +10,11 @@
 // @match       https://www.douban.com/people/*
 // @match       https://v.youku.com/*
 // @match       https://www.ixigua.com/*
-// @description 支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频
+// @match       https://www.huxiu.com/*
+// @description 支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频、虎嗅
 // @description 默认支持F键全屏的网站：B站、新浪微博
 // @license MIT
-// @description 2022-12-10
+// @description 2022-12-28
 // ==/UserScript==
 
 // 获取网站名称 @example: getSiteName('www.baidu.com'); // 'baidu'
@@ -30,7 +31,7 @@ const getSiteName = hostname => hostname.match(/([^\.]+)\.[^\.]+$/)[1];
           document.querySelector(".dplayer-icon.dplayer-full-icon").click();
         }
         if(siteName === 'aliyundrive'){
-          document.querySelector(".dplayer-icon.dplayer-full-icon").click();
+          document.querySelector(".action--3G9se").click();
         }
         if(siteName === 'miguvideo'){
           document.querySelector(".btn-bg.zoom-out").click();
@@ -45,6 +46,9 @@ const getSiteName = hostname => hostname.match(/([^\.]+)\.[^\.]+$/)[1];
           var arr1 = document.getElementsByClassName("xgplayer-control-item__entry");
           arr1[arr1.length-1].click();
          //取最后一个元素
+        }
+        if(siteName === 'huxiu'){
+          document.getElementsByClassName("plyr__control")[3].click();
         }
     }
 }
