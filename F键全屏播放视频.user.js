9fa0703a4941c62f429f707add456c482b37ada5
metaldudu <metaldudu@gmail.com>
2023-02-03 11:09:40 +0800

    toutiao

--- F键全屏播放视频.user.js
+++ F键全屏播放视频.user.js
@@ -2,7 +2,7 @@
 // @name        F键全屏播放视频
 // @namespace   Violentmonkey Scripts
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAB7UlEQVRoge2ZTU4CMRiGHxF0dGPUxJ16A6/hT1x4ARGDGGNcaryBW0/gKVAuocYDmICCrt2aAcRFS0SkYGk/Z2L6JN9i6LTf+9KfTFsIBAI+mAFKQAV4AZpARyiaOkdF54x8mdgG6oLCR8UzsOVq4gRoJ2iiGy3geFwT2ykx0Wtm09ZEhOrSpMX3RwM1X3/NYQpEm+JgkOCMwciOjes/ZqA2k5E1QSGuDNQ2YXg5BnJyWpxoAlP9P5qMdGS1OPNDt2louXAFLPwiHnwmzfpsTPMOvAFzwOKQ9yZ9JpUw0mUfuBRs/xsSQysRgpG08W+MSE72G+DVULYOFH0mkzTyqGMQS76TSRo5Ai4MZdO+k0kaiYB5wfa/8W8mezCSNoKRIXQnudUhgSthY5U2gpG0EYykjVFG0rR6DdUyykgJ+PCnZWw+UFqsiVH/QBbYAKokd2hdRW3Ecvo5tjHS0JWW9fMskAeugVqPUYmIdY4ysMvXF8KqLq/bGLnRlfI2lYQpoDSVbSqVdKVbzJ8xf0kGuEdpstrr995YnfrXZc05SkuNMbbJW6h7uzZwRjI9k0GZaGst6+M2dKwb6AB3wB6wguzdSU7nKPA1nFqowwwnNoEnklt+azj0RD8R6hKyjFqapZffhs5VRODoKBCw4BPhMH4TDNDMWAAAAABJRU5ErkJggg==
-// @version     0.1.5
+// @version     0.1.6
 // @author      MetalDudu
 // @match       https://*.thepaper.cn/*
 // @match       https://www.aliyundrive.com/*
@@ -12,10 +12,11 @@
 // @match       https://www.ixigua.com/*
 // @match       https://www.huxiu.com/*
 // @match       https://open.163.com/*
-// @description 支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频、虎嗅、网易公开课
+// @match       https://www.toutiao.com/*
+// @description 支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频、虎嗅、网易公开课、头条
 // @description 默认支持F键全屏的网站：B站、新浪微博
 // @license MIT
-// @description 2023-01-03
+// @description 2023-02-03
 // ==/UserScript==
 
 // 获取网站名称 @example: getSiteName('www.baidu.com'); // 'baidu'
@@ -37,7 +38,13 @@ const getSiteName = hostname => hostname.match(/([^\.]+)\.[^\.]+$/)[1];
         if(siteName === 'miguvideo'){
           document.querySelector(".btn-bg.zoom-out").click();
         }
-        if(siteName === 'douban' || '163'){
+        if(siteName === 'toutiao'){
+          document.querySelector(".xgplayer-fullscreen").click();
+        }
+        if(siteName === 'douban'){
+          document.getElementsByClassName("vjs-fullscreen-control vjs-control vjs-button")[0].click();
+        }
+        if(siteName === '163'){
           document.getElementsByClassName("vjs-fullscreen-control vjs-control vjs-button")[0].click();
         }
         if(siteName === 'youku'){
@@ -50,7 +57,6 @@ const getSiteName = hostname => hostname.match(/([^\.]+)\.[^\.]+$/)[1];
         }
         if(siteName === 'huxiu'){
           document.getElementsByClassName("plyr__control")[3].click();
-
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FullScreen-hotkey
 
-使用 F 快捷键全屏播放视频，支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频、虎嗅、网易公开课
+使用 F 快捷键全屏播放视频，支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频、虎嗅、网易公开课、头条
 
 [https://greasyfork.org/zh-CN/scripts/455333](https://greasyfork.org/zh-CN/scripts/455333)
