3ff8d611e3d202352d9dd86b1694c63186b93b76
metaldudu <metaldudu@gmail.com>
2023-10-03 21:48:55 +0800

    fix aliyun

--- F键全屏播放视频.user.js
+++ F键全屏播放视频.user.js
@@ -2,7 +2,7 @@
 // @name        F键全屏播放视频
 // @namespace   Violentmonkey Scripts
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAB7UlEQVRoge2ZTU4CMRiGHxF0dGPUxJ16A6/hT1x4ARGDGGNcaryBW0/gKVAuocYDmICCrt2aAcRFS0SkYGk/Z2L6JN9i6LTf+9KfTFsIBAI+mAFKQAV4AZpARyiaOkdF54x8mdgG6oLCR8UzsOVq4gRoJ2iiGy3geFwT2ykx0Wtm09ZEhOrSpMX3RwM1X3/NYQpEm+JgkOCMwciOjes/ZqA2k5E1QSGuDNQ2YXg5BnJyWpxoAlP9P5qMdGS1OPNDt2louXAFLPwiHnwmzfpsTPMOvAFzwOKQ9yZ9JpUw0mUfuBRs/xsSQysRgpG08W+MSE72G+DVULYOFH0mkzTyqGMQS76TSRo5Ai4MZdO+k0kaiYB5wfa/8W8mezCSNoKRIXQnudUhgSthY5U2gpG0EYykjVFG0rR6DdUyykgJ+PCnZWw+UFqsiVH/QBbYAKokd2hdRW3Ecvo5tjHS0JWW9fMskAeugVqPUYmIdY4ysMvXF8KqLq/bGLnRlfI2lYQpoDSVbSqVdKVbzJ8xf0kGuEdpstrr995YnfrXZc05SkuNMbbJW6h7uzZwRjI9k0GZaGst6+M2dKwb6AB3wB6wguzdSU7nKPA1nFqowwwnNoEnklt+azj0RD8R6hKyjFqapZffhs5VRODoKBCw4BPhMH4TDNDMWAAAAABJRU5ErkJggg==
-// @version     0.1.6
+// @version     0.1.7
 // @author      MetalDudu
 // @match       https://*.thepaper.cn/*
 // @match       https://www.aliyundrive.com/*
@@ -16,7 +16,7 @@
 // @description 支持网站：澎湃、阿里云盘、咪咕视频、豆瓣广播、优酷、西瓜视频、虎嗅、网易公开课、头条
 // @description 默认支持F键全屏的网站：B站、新浪微博
 // @license MIT
-// @description 2023-02-03
+// @description 2023-10-03
 // ==/UserScript==
 
 // 获取网站名称 @example: getSiteName('www.baidu.com'); // 'baidu'
@@ -33,7 +33,7 @@ const getSiteName = hostname => hostname.match(/([^\.]+)\.[^\.]+$/)[1];
           document.querySelector(".dplayer-icon.dplayer-full-icon").click();
         }
         if(siteName === 'aliyundrive'){
-          document.querySelector(".action--3G9se").click();
+          document.querySelector(".action--HeWYA").click();
         }
         if(siteName === 'miguvideo'){
           document.querySelector(".btn-bg.zoom-out").click();
